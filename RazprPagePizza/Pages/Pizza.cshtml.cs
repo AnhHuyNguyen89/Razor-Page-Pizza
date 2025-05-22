9afe23357f1a68e3f6a589fe78546c419e16c9ef
Anh <nguay089@mymail.unisa.edu.au>
2025-05-22 17:28:09 +0930

    Add project files.

---
+++ RazprPagePizza/Pages/Pizza.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazprPagePizza.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
